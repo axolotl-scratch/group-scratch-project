06d04714fab2e2db93df0be820b0009c83576629
Michael Prince <89811674+MichaelGPrince@users.noreply.github.com>
2021-10-16 16:55:38 -0600

    Revert "frontend files"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Header from "./Header.js";
-import LoginContainer from "./login/LoginContainer.js";
-import Modal from "./modal/Modal.js";
-import Button from "./Button.js";
+import Header from './Header.js';
+import loginContainer from './loginContainer.js';
+
+
 
 function App() {
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+
   // on submit of button
-  const onSubmit = (e) => {
-    console.log(e);
-    // console.log(username);
-    // console.log(password);
-  };
+  const onSubmit = () => {
 
-  const homepageText = "MEIN GOTT MUSS DAS SEIN";
+  }
 
-  const loginButtonText = "Login to Spotify";
-
-  const createPlaylistText = "Create Playlist";
 
   return (
     <div className="app">
       <h1 id="appHeader">Festival Playlistify</h1>
-{/* 
-      <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/modal">Modal</Link>
-          </li>
+      <Header />
 
-        </ul>
-      </div>
-
-        <Switch>
-          <Header/>
-          <Route exact path="/login">
-            <LoginContainer onSubmit={onSubmit} text={loginButtonText} />
-          </Route>
-
-          <Route exact path="/modal">
-            <Modal text={createPlaylistText} />
-          </Route>
-
-          <Route exact path="/">
-            <form>
-              <Button text={homepageText} />
-            </form>
-          </Route>
-          
-        </Switch>
-      </Router> */}
+      <loginContainer />
     </div>
-  );
+  )
 }
 
 export default App;
