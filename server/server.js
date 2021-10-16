3367fda462add50ff283f8e82c79f94a18704c10
MichaelGPrince <mgp2454@gmail.com>
2021-10-16 14:32:17 -0700

    added controllers boilerplate & mapped out routers
    Co-authored-by: Chris Tang <chrisjtang@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,11 +1,82 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 
-// statically serve everything in the public folder (bundle.js and index.html)
+// spotify api client id, secret, and redirect uri 
+const client_id = 'e6844aab50c045bcaeeefe104bef4f4b'; // Your client id
+const client_secret = 'cb60640e4a2146919f50e7d2fc1e3a56'; // Your secret
+const redirect_uri = 'http://localhost:3000/modal'; // Your redirect uri.  in our case: '/modal'
+
+// random sring and number generator
+const generateRandomString = function(length) {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
+// statically serve everything in the public folder (bundle.js and index.html). 
+// cors() sets default headers to {
+//   "origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// }
+// cookieParser middleware parses cookies attached to the client request object
 app.use(express.static(path.join(__dirname, '../public')))
+	.use(cors())
+	.use(cookieParser());
+
+app.use('/', (req, res) => {
+	// this should display the option to go to the landing page/login page
+});
+
+app.use('/login', (req, res) => {
+	// redirect to oauth
+	const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization.  redirecting to the spotify authorization page
+  const scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.use('/modal', 
+	// this should display the "create playlist" page (GET)
+	// should create a new playlist with the name entered in the input field (POST)
+	createPlaylist,
+	(req, res) => {
+	// redirect to the main page
+});
+
+app.use('/compile', 
+	// this should display the main page (GET)
+	// search for an artist(s) ID (GET)
+	searchArtist,
+	// search for the artist(s)'s top 10 songs (GET)
+	getTopTen,
+	// add the top 10 songs to the playlist (PUT)
+	addSongsToPlaylist,
+	(req, res) => {
+		// what do here? confirmation page? redirect to...???
+});
 
 
 // ******error handlers*******
